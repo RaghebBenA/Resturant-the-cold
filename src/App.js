6eecefda037703f9599ae5bcbc83e806f8a36117
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-02-02 19:56:28 +0100

    adding the MenComonent to the app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
-import {Navbar , NavbarBrand} from 'reactstrap';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-  <Navbar dark color="info">
-  <div className="container">
-  <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-  </div>
-  </Navbar>
+      <div>
+        <Navbar dark color="info">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
       </div>
     );
   }
