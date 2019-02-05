fdb7838079551fc567447c0dbb34a86837b2f9ae
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-02-05 22:38:53 +0100

    tranform the dishdetails component and MenuComonent to functional component

--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react';
-import {CardText, CardBody, Card, CardImg, CardTitle} from "reactstrap";
+import React from 'react';
+import { CardText, CardBody, Card, CardImg, CardTitle } from "reactstrap";
 
 
-class Details extends Component {
-   
-    renderComments(comments) {
+
+
+
+    function RenderComments ({comments}){
         if (comments == null) {
             return (<div></div>)
         }
@@ -15,10 +16,10 @@ class Details extends Component {
                     <p>-- {comment.author},
                     &nbsp;
                     {new Intl.DateTimeFormat('en-US', {
-                            year: 'numeric',
-                            month: 'long',
-                            day: '2-digit'
-                        }).format(new Date(comment.date))}
+                        year: 'numeric',
+                        month: 'long',
+                        day: '2-digit'
+                    }).format(new Date(comment.date))}
                     </p>
                 </li>
             )
@@ -34,7 +35,8 @@ class Details extends Component {
         )
     }
 
-    renderDish(dish) {
+     function RenderDish ({dish}) {
+
         if (dish != null) {
             return (
                 <Card className="col-10 col-md-5 m-1">
@@ -51,22 +53,23 @@ class Details extends Component {
             )
         }
     }
-    render(){
-        const dish = this.props.dish
-        if (dish == null) {
-            return (<div></div>)
+      const Details = (props) =>{
+        
+        if (props.dish != null) {
+        return (
+            <div className="container">
+                <div className="row">
+                   <RenderDish dish={props.dish}/>
+                   <RenderComments comments={props.dish.comments} />
+                   </div>
+            </div>
+        )
         }
-        const dishItem = this.renderDish(this.props.dish)
-        const commentItem = this.renderComments(this.props.dish.comments)
-return (
-    <div className="container">
-   <div className="row">
-   {dishItem}
-   {commentItem}
-   </div>
-    </div>
-)
-    }
-}
+        else {
+            return (
+                <div></div>
+            )
+        }
+        }
 
-export default Details ;
+export default Details;
--- src/components/MainCompenet.js
+++ src/components/MainCompenet.js
@@ -6,34 +6,34 @@ import { DISHES } from "../Share/dishes"
 
 
 class Main extends Component {
-  constructor(){
-    super()
-    this.state ={
-      dishes : DISHES,
-      selectedDish: null
+    constructor() {
+        super()
+        this.state = {
+            dishes: DISHES,
+            selectedDish: null
+        }
     }
-  }
-  onDishSelect(dishID) {
-    this.setState({
-        selectedDish: dishID
-    })
-}
-  render() {
-    return (
-      <div>
-        <Navbar dark color="info">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
-        <Menu dishes={this.state.dishes} onClick={(dishID) => this.onDishSelect(dishID)} />
-        <Details 
-        dish={this.state.dishes.filter(dish => { return dish.id === this.state.selectedDish })[0]}
-        />
-       
-      </div>
-    );
-  }
+    onDishSelect(dishID) {
+        this.setState({
+            selectedDish: dishID
+        })
+    }
+    render() {
+        return (
+            <div>
+                <Navbar dark color="info">
+                    <div className="container">
+                        <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu dishes={this.state.dishes} onClick={(dishID) => this.onDishSelect(dishID)} />
+                <Details
+                    dish={this.state.dishes.filter(dish => { return dish.id === this.state.selectedDish })[0]}
+                />
+
+            </div>
+        );
+    }
 }
 
 export default Main;
