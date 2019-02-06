6423ef09b11dd5d03675909c5c6463ae1b59f8c9
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-02-06 16:57:55 +0100

    work

--- src/components/MainCompenet.js
+++ src/components/MainCompenet.js
@@ -16,7 +16,7 @@ class Main extends Component {
     }
     onDishSelect(dishID) {
         this.setState({
-            selectedDish: dishID
+            selectedDish: dishID 
         })
     }
     render() {
