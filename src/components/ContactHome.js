9fc080a03aca549815fb9c91981f6c8448c62b02
RaghebBenA <ragheb.pr.2014@gmail.com>
2019-02-08 19:12:34 +0100

    Adding comment post to the menu items

--- src/components/ContactHome.js
+++ src/components/ContactHome.js
@@ -25,7 +25,6 @@ class Contact extends Component {
     }
 
 
-
     render() {
 
         return (
--- src/components/DishDetails.js
+++ src/components/DishDetails.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { CardText, CardBody, Card, CardImg, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button,
+     Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap'
 import { Link } from 'react-router-dom';
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 
 
@@ -23,6 +25,7 @@ function RenderComments({comments}) {
             <ul className="list-unstyled">
                 {commentList}
             </ul>
+            <CommentForm />
         </div>
     );
 }
@@ -32,7 +35,7 @@ function RenderDish({ dish }) {
 
     if (dish != null) {
         return (
-            <Card className="col-10 col-md-5 m-1">
+            <Card >
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardBody>
                     <CardTitle>{dish.name}</CardTitle>
@@ -51,21 +54,25 @@ const Details = (props) => {
     if (props.dish != null) {
         return (
             <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to='/menu'>Menu</Link> </BreadcrumbItem>
-                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
-                    </Breadcrumb>
-                </div>
+            <div className="row">
+                <Breadcrumb>
+                    <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                    <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                </Breadcrumb>
                 <div className="col-12">
-                <h3>{props.dish.name}</h3>
-                <hr />
+                    <h3>{props.dish.name}</h3>
+                    <hr/>
+                </div>
             </div>
-                <div className="row">
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
                     <RenderDish dish={props.dish} />
+                </div>
+                <div className="col-12 col-md-5 m-1">
                     <RenderComments comments={props.comments} />
                 </div>
             </div>
+        </div>
         )
     }
     else {
@@ -76,3 +83,86 @@ const Details = (props) => {
 }
 
 export default Details;
+
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !(val) || (val.length <= len);
+const minLength = (len) => (val) => val && (val.length >= len);
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isModalOpen: false
+        };
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    toggleModal() {
+        this.setState({ isModalOpen: !this.state.isModalOpen });
+    }
+
+    handleSubmit(values){
+        this.toggleModal();
+
+        console.log('comment:', values);
+        alert('comment:' + JSON.stringify(values));
+    }
+
+
+    render() {
+        return (
+            <div>
+                <Button outline onClick={this.toggleModal}>
+                    <span className="fa fa-pencil fa-lg"> Submit comment</span>
+                </Button>
+
+                <div className="row row-content">
+                    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                        <ModalHeader toggle={this.toggleModal}> Submit comment</ModalHeader>
+                        <ModalBody>
+                            <div className="col-12 col-md-9">
+                                <LocalForm onSubmit={(values) => this.handleSubmit(values)} >
+                                    <Row className="form-group">
+                                        <Label htmlFor="rating">Rating</Label>
+                                        <Col md={10}>
+                                            <Control.select model=".rating" name="rating" className="form-control" >
+                                                <option>1</option>
+                                                <option>2</option>
+                                                <option>3</option>
+                                                <option>4</option>
+                                                <option>5</option>
+                                            </Control.select>
+                                        </Col>
+                                    </Row>
+
+                                    <Row className="form-group">
+                                        <Label htmlFor="author" md={2}>Your name</Label>
+                                        <Col md={10}>
+                                            <Control.text model=".author" id="author" name="author" placeholder="Author" className="form-control" validators={{ required, minLength: minLength(3), maxLength: maxLength(15) }} />
+                                            <Errors className="text-danger" model=".author" show="touched" messages={{ required: 'Required', minLength: 'Must be greater than 3 characters', maxLength: 'Must be 15 charaters or less' }} />
+                                        </Col>
+                                    </Row>
+
+                                    <Row className="form-group">
+                                        <Label htmlFor="feedback" md={2}>Your feedback</Label>
+                                        <Col md={10}>
+                                            <Control.textarea model=".comment" id="comment" name="comment" rows="6" className="form-control" validators={{ required }} />
+                                            <Errors className="text-danger" model=".comment" show="touched" messages={{ required: 'Required' }} />
+                                        </Col>
+                                    </Row>
+
+                                    <Button type="submit" value="submit" color="primary">Submit</Button>
+                                </LocalForm>
+                            </div>
+                        </ModalBody>
+                    </Modal>
+                </div>
+            </div>
+      
+     )
+    }
+    }
